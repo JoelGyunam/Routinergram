4798a5d32f2b5b673dbd6fb815be92c467a6b074
Joelinseoul <gynpark@gmail.com>
2023-08-01 21:19:43 +0900

    프로젝트 파일 및 환경설정 최초 등록

---
+++ src/test/java/com/routinergram/RoutinergramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routinergram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutinergramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
